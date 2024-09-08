932c1831bb88261cbca4484112dfb513a6b3c7ac
Mateus Nadal <nadalmateus@hotmail.com>
2024-09-08 20:30:59 -0300

    add OpenAPI support

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,5 +1,11 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-WebApplication app = builder.Build();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => x.CustomSchemaIds(n => n.FullName));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
